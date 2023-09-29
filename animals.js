7ec150464f7547c1eb2aed8c84c42c2ec0d519e4
Chazzy11 <charlotterogers96@hotmail.com>
2023-09-29 11:56:11 +0100

    animals.js set up and animals.json set up

--- animals.js
+++ animals.js
@@ -1,1 +1,8 @@
 import {promises as fs} from 'node:fs'; // to be able to read/write and work with async functions
+import path from "node:path" // providing utilities for working with file and directory paths
+
+import { v4 as uuidv4 } from "uuid" // importing the id function from 3rd party package
+
+const filePath = path.resolve(process.cwd(), "animals.json") // creating an absolute path so we use same path no matter where we are in project
+
+// 
