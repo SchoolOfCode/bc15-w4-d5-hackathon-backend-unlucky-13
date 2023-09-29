688e10a2cd04cd0ffb44ef83248f6576ef4cb66c
Chazzy11 <charlotterogers96@hotmail.com>
2023-09-29 12:18:28 +0100

    addAnimal function written

--- animals.js
+++ animals.js
@@ -5,4 +5,21 @@ import { v4 as uuidv4 } from "uuid" // importing the id function from 3rd party 
 
 const filePath = path.resolve(process.cwd(), "animals.json") // creating an absolute path so we use same path no matter where we are in project
 
-// 
+// Function to first construct the animal object, and then add it
+
+async function addAnimal (animalName, fact, habitat) {
+   const animalsJSON = await fs.readFile(filePath, "utf-8"); // reading the JSON file
+   const animalList = JSON.parse(animalsJSON); // putting the JSON into format that JS can read - like a translator 
+   // creating the new animal:
+    const newAnimal = {
+      id: uuidv4(),
+      animalName,
+      fact,
+      habitat,
+   }
+// adds the animal to the end of the array in animals.JSON:
+animalList.push(newAnimal);
+// translating the data back into json format
+await fs.writeFile(filePath, JSON.stringify(animalList, null, 3), "utf-8"); // stringify syntax: the value, the replacer, and then amount of white space
+return newAnimal;
+}
